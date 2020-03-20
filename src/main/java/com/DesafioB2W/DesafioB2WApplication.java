3f3d4b878d98ca0219f5fbe412b3c79d0a1aff6f
Pedro Henrique <pedrohenrique.midas@gmail.com>
2020-03-20 09:56:37 -0300

    Commit inicial Desafio B2W

---
+++ src/main/java/com/DesafioB2W/DesafioB2WApplication.java
@@ -0,0 +1,13 @@
+package com.DesafioB2W;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioB2WApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioB2WApplication.class, args);
+	}
+
+}
