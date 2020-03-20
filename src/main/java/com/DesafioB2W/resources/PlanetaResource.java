e390122f7e4eb3d3ac20b5833abf8c09f4075cda
Pedro Henrique <pedrohenrique.midas@gmail.com>
2020-03-20 14:02:34 -0300

    Usando padrao DTO para retornar os Planetas

--- src/main/java/com/DesafioB2W/resources/PlanetaResource.java
+++ src/main/java/com/DesafioB2W/resources/PlanetaResource.java
@@ -1,8 +1,7 @@
 package com.DesafioB2W.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.DesafioB2W.domain.Planeta;
+import com.DesafioB2W.dto.PlanetaDTO;
 import com.DesafioB2W.services.PlanetaService;
 
 @RestController
@@ -22,8 +22,9 @@ public class PlanetaResource {
 	
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<Planeta>> findall(){
+	public ResponseEntity<List<PlanetaDTO>> findall(){
 		List<Planeta> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<PlanetaDTO> listdto = list.stream().map(x -> new PlanetaDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listdto);
 	}
 }
